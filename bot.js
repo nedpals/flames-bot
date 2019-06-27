2acf91368cd33820e99d5d3db5847608dbf4a5e5
Ned Palacios <npdoesmc@gmail.com>
2019-06-27 16:48:49 +0800

    updated dist and deps

--- bot.js
+++ bot.js
@@ -37,32 +37,29 @@ class FlamesBot extends disco_js_1.Bot {
             }
             // Convert mention tags into user arrays.
             const users = this.getUsersFromMentions(...args);
-            const printNames = `Users who gets FLAMES'd: ${users.map(user => `<@${user.id}>`).join(", ")}\n`;
+            const printNames = `Users who gets FLAMES'd: ${users.map(user => user.id !== "0-non" ? `<@${user.id}>` : user.username).join(", ")}\n`;
             // This indicates that the bot recognized the message of the user.
             const startMsg = yield message.channel.send(dedent `
             ${printNames}
-
             Guess the outcome by reacting to this message.\n
-            \:regional_indicator_f: - Friends\n
-            \:regional_indicator_l: - Lovers\n
-            \:regional_indicator_a: - Anger\n
-            \:regional_indicator_m: - Married\n
-            \:regional_indicator_e: - Enemies\n
-            \:regional_indicator_s: - Soulmates\n
-
+            ${flames_emojis["friends"]} - Friends\n
+            ${flames_emojis["lovers"]} - Lovers\n
+            ${flames_emojis["anger"]} - Anger\n
+            ${flames_emojis["married"]} - Married\n
+            ${flames_emojis["enemies"]} - Enemies\n
+            ${flames_emojis["soulmates"]} - Soulmates\n
             You have a minute to guess!
         `);
             const computed = (() => {
                 const usernames = users.map(u => u.username.toLowerCase());
-                const discriminator = users.map(u => u.discriminator);
                 // Results object
                 let sum = 0;
                 // Splits usernames into an array of characters
                 const usernamesChopped = usernames.map(u => u.split(""));
                 // This is where intersecting/common characters between two usernames go.
-                let common = [];
+                const common = [];
                 // Loops each character of the first username
-                for (const letter in usernamesChopped[0]) {
+                usernamesChopped[0].forEach(letter => {
                     // Checks if the character in first username is present in second username
                     if (usernamesChopped[1].includes(letter)) {
                         // If character is not present in the common array, push it.
@@ -70,15 +67,16 @@ class FlamesBot extends disco_js_1.Bot {
                             common.push(letter);
                         }
                     }
-                }
+                });
                 // Filter usernames
-                let commonCharRemoved = usernamesChopped.map(name => {
-                    return name.filter(c => !common.includes(c));
+                const commonCharRemoved = usernamesChopped.map(name => {
+                    return name.filter(c => !(common.length === 0 ? [] : common).includes(c));
                 });
                 // Combines the sum of two char lengths of the remaining chars in the usernames
                 sum = commonCharRemoved.map(u => u.length).reduce((p, c) => p + c, 0);
+                const result = spin_1.default(sum || 0);
                 // SPIN THAT WHEEL!
-                return spin_1.default(sum);
+                return result;
             })();
             yield delay(60000);
             // Collect people who reacted to the initial message.
@@ -93,9 +91,7 @@ class FlamesBot extends disco_js_1.Bot {
             // Prints the result
             yield startMsg.channel.send(dedent `
         ${printNames}
-
         The result is: ${flames_emojis[computed]} ${computed.toUpperCase()}!\n
-
         ${reactors.length !== 0 ? `
         Congratulations to those people who guessed the result:\n
         ${reactors.map(u => `<@${u.id}>`).join("\n")}
@@ -110,7 +106,9 @@ class FlamesBot extends disco_js_1.Bot {
             __**What does it do?**__\n
             It predicts the romantic chemistry/relationship between the names of the two people by\nremoving the common letters and computes the remaining ones.\n
             \n
-            Usage: \`+flames @username1#1234 @username2#2345\`
+            Usage:\n
+            \`\`\`+flames @username1#1234 @username2#2345\`\`\`\n
+            **Tip:** You can use ordinary names.
         `);
             }
             else {
@@ -122,7 +120,7 @@ class FlamesBot extends disco_js_1.Bot {
         };
     }
     ready() {
-        console.log("Hello!");
+        console.log("🔥 F.L.A.M.E.S Bot has started 🔥");
     }
     getUsersFromMentions(...mentions) {
         if (!mentions)
@@ -134,6 +132,14 @@ class FlamesBot extends disco_js_1.Bot {
                     mention = mention.slice(1);
                 }
                 return this.client.users.get(mention);
+            }
+            else {
+                return {
+                    username: mention,
+                    discriminator: "0000",
+                    bot: false,
+                    id: "0-non"
+                };
             }
         });
     }
--- src/spin.js
+++ src/spin.js
@@ -11,27 +11,32 @@ function spin(idx_number, debug = false) {
     };
     const arr = Object.keys(flames_obj);
     let result;
-    const remainder = idx_number % arr.length;
-    const groups = Math.ceil(idx_number / arr.length);
-    [...Array(groups).keys()].forEach((group, g_i, gs_arr) => {
-        [...arr.keys()].forEach((i, _, g_arr) => {
-            if (debug) {
-                console.log(`Group: ${group}`, i, arr[i]);
-            }
-            if (g_i === gs_arr.length - 1 && i === g_arr.length - 1) {
-                result = arr[i];
-            }
+    if (idx_number === 0) {
+        result = arr[0];
+    }
+    else {
+        const remainder = idx_number % arr.length;
+        const groups = Math.ceil(idx_number / arr.length);
+        [...Array(groups).keys()].forEach((group, g_i, gs_arr) => {
+            [...arr.keys()].forEach((i, _, g_arr) => {
+                if (debug) {
+                    console.log(`Group: ${group}`, i, arr[i]);
+                }
+                if (g_i === gs_arr.length - 1 && i === g_arr.length - 1) {
+                    result = arr[i];
+                }
+            });
         });
-    });
-    if (remainder !== 0) {
-        [...Array(remainder).keys()].forEach((i, _, r_arr) => {
-            if (debug) {
-                console.log(`Remainder`, i, arr[i]);
-            }
-            if (i === r_arr.length - 1) {
-                result = arr[i];
-            }
-        });
+        if (remainder !== 0) {
+            [...Array(remainder).keys()].forEach((i, _, r_arr) => {
+                if (debug) {
+                    console.log(`Remainder`, i, arr[i]);
+                }
+                if (i === r_arr.length - 1) {
+                    result = arr[i];
+                }
+            });
+        }
     }
     return flames_obj[result];
 }
